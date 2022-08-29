a207dee50b61b89db90705a08e57eefeac48c8e6
wakenedo <a_nuner@outlook.com>
2022-08-29 20:18:08 -0300

    Redux-Persist Installed

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,50 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Box, Button, Typography } from '@mui/material'
-import { useDispatch } from 'react-redux'
-import { addToCart } from '../../redux/cartSlice'
+import { Box, Typography } from '@mui/material'
 
 import CartItem from './CartItem'
 
 const Cart = () => {
-  const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)
 
 
   return (
     <Box>
-      <Button
-        variant='contained'
-        onClick={() =>
-          dispatch(addToCart({
-
-          }))
-        }
-        sx={{
-          fontSize: {
-            lg: '13px',
-            md: '18px',
-            xs: '14px',
-          }
-        }}
-      >
-        <Box
-          sx={{
-            marginTop: {
-              lg: '',
-              md: '5px',
-              xs: '',
-            },
-            marginRight: {
-              lg: '',
-              md: '10px',
-              xs: '',
-            }
-          }}
-        >
-        </Box>
-        Add to Cart
-      </Button>
 
       <Typography>
         Are you ready to purchase these?
