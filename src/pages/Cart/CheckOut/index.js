eb532c93691d70ad41f272b12dc81781b9a5ce7e
wakenedo <a_nuner@outlook.com>
2022-08-29 23:44:31 -0300

    Cart - Part 3

--- src/pages/Cart/CheckOut/index.js
+++ src/pages/Cart/CheckOut/index.js
@@ -1,58 +1,24 @@
 import React from 'react'
-import { Box, Button, Typography } from '@mui/material'
+import { Box } from '@mui/material'
+import CheckOutNow from './CheckOutNow'
+import CheckOutProducts from './CheckOutProducts'
 
-const CheckOut = () => {
+const CheckOut = ({ cart }) => {
   return (
     <Box
       backgroundColor='#1CFBD3'
+      justifyContent='center'
+      sx={{
+        display: {
+          lg: '',
+          md: 'flex',
+          xs: '',
+        }
+      }}
     >
-      <Box>
-        <Box
-          display='flex'
-          alignItems='center'
-        >
-          <Box
-            sx={{
-              marginTop: {
-                lg: '',
-                md: '6px',
-                xs: '',
-              }
-            }}
-          >
-            <Typography
-              color='#4B4B4B'
-              sx={{
-                fontSize: {
-                  lg: '',
-                  md: '12px',
-                  xs: '',
-                }
-              }}
-            >
-              TOTAL :
-            </Typography>
-          </Box>
-          <Typography
-            fontWeight='bold'
-            color='#4B4B4B'
-            sx={{
-              fontSize: {
-                lg: '',
-                md: '20px',
-                xs: '',
-              }
-            }}
-          >
-            120,00 USD
-          </Typography>
-        </Box>
-        <Button
-          variant='contained'
-        >
-          Check Out Now
-        </Button>
-      </Box>
+
+      <CheckOutProducts />
+      <CheckOutNow />
     </Box>
   )
 }
