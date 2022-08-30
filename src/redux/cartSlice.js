9df5c5ea3482ecd8cd418633394394de65298b27
wakenedo <a_nuner@outlook.com>
2022-08-30 17:50:16 -0300

    Cart - Part 5
    Dealing with the CartItems, removing , increasing and decreasing the quantity of each item and removing the item when they reach 0
    
    Had to adapt some of the reducer logic.

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -20,8 +20,9 @@ const cartSlice = createSlice({
     },
     decrementQuantity: (state, action) => {
       const item = state.cart.find((item) => item.id === action.payload);
-      if (item.quantity === 1) {
-        item.quantity = 1
+      if (item.quantity === 0) {
+        const removeItem = state.cart.filter((item) => item.id !== action.payload);
+        state.cart = removeItem
       } else {
         item.quantity--;
       }
