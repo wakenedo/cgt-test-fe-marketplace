9f6d77ec06fe1c47039495a1b4b61ad01f8197e3
wakenedo <a_nuner@outlook.com>
2022-09-01 20:19:53 -0300

    Update index.js

--- src/pages/Product/ProductName/index.js
+++ src/pages/Product/ProductName/index.js
@@ -1,24 +1,43 @@
-import React from 'react'
-import { Typography } from '@mui/material'
+import React, { useContext } from 'react'
+import { Box, Typography } from '@mui/material'
+import { DarkModeContext } from '../../../context'
 
 
 
 const ProductName = ({ products, id }) => {
+  const { darkMode } = useContext(DarkModeContext)
   return (
-    <Typography
-      fontWeight='bold'
-      color='#4B4B4B'
+    <Box
       sx={{
-        fontSize: {
-          lg: '55px',
-          md: '45px',
-          xs: '25px',
+        margin: {
+          lg: '10px',
+          md: '10px',
+          xs: '5px',
+        },
+        padding: {
+          lg: '10px',
+          md: '10px',
+          xs: '5px',
         }
       }}
     >
-      {products[id]?.title}
-    </Typography>
+      <Typography
+        fontWeight='bold'
+        color='#4B4B4B'
+        sx={{
+          color: darkMode ? '#E4E4E4' : '#4B4B4B',
+          fontSize: {
+            lg: '55px',
+            md: '45px',
+            xs: '25px',
+          }
+        }}
+      >
+        {products[id]?.title}
+      </Typography>
+    </Box>
   )
+
 }
 
 export default ProductName
